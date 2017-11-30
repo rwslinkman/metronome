aa676c969d0d1685272ec9e1b2fd9a5212268980
Rick Slinkman <rick.slinkman@philips.com>
2017-11-30 22:18:04 +0100

    Extended TestFile to have more information about a file.

--- src/Metronome/Util/TestFile.php
+++ src/Metronome/Util/TestFile.php
@@ -11,11 +11,28 @@ class TestFile extends UploadedFile
 {
     private $pathName;
     private $mime;
+    private $extension;
+    private $baseName;
+    private $modifiedDate;
 
-    public function __construct($originalName, $mimeType = null, $size = null, $error = null) {
+    /**
+     * TestFile constructor.
+     * @param string $originalName
+     * @param string $mimeType
+     * @param int $size
+     * @param string $error
+     * @param string $extension
+     * @param string $baseName
+     * @param int $modifiedDate
+     */
+    public function __construct($originalName, $mimeType = null, $size = null, $error = null,
+                                $extension = ".ext", $baseName = "testFile", $modifiedDate = 1512076615) {
         parent::__construct("/path/to/".$originalName, $originalName, $mimeType, $size, $error ?: "test-file", true);
         $this->pathName = "/path/to/".$originalName;
         $this->mime = $mimeType;
+        $this->extension = $extension;
+        $this->baseName = $baseName;
+        $this->modifiedDate = $modifiedDate;
     }
 
     public function getPathname()
@@ -35,9 +52,13 @@ class TestFile extends UploadedFile
 
     public function toBrowsable() {
         $file = array();
-        $file['relativePathName'] = $this->getClientOriginalName();
-        $file['pathName'] = $this->pathName;
-        $file['name'] = $this->getFilename();
+        $file['relativePathName']   = $this->getClientOriginalName();
+        $file['pathName']           = $this->pathName;
+        $file['name']               = $this->getFilename();
+        $file['extension']          = $this->extension;
+        $file['baseName']           = $this->baseName;
+        $file['modifiedDate']       = $this->modifiedDate;
+        $file['size']               = $this->getSize();
         return $file;
     }
 }
