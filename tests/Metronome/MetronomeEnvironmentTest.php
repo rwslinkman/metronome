e26709e0e36069c0dc2f4ed9886f23ffc349cb24
Rick Slinkman <rick.slinkman@philips.com>
2017-12-31 18:17:50 +0100

    Mock client.

--- tests/Metronome/MetronomeEnvironmentTest.php
+++ tests/Metronome/MetronomeEnvironmentTest.php
@@ -1,10 +1,17 @@
 <?php
 namespace Metronome;
 
-class MetronomeEnvironmentTest extends \PHPUnit_Framework_TestCase
+use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MetronomeEnvironmentTest extends WebTestCase
 {
     public function test_example() {
-        $env = new MetronomeEnvironment(null);
+        /** @var Client $clientMock */
+        $clientMock = \Mockery::mock('\Symfony\Bundle\FrameworkBundle\Client', array(
+            'getContainer' => array()
+        ));
+        $env = new MetronomeEnvironment($clientMock);
         $this->assertNotNull($env);
     }
 }
