7be42716c2c37c6333925048f04b3d5d505bc06d
Rick Slinkman <rw.slinkman@gmail.com>
2019-10-07 23:54:14 +0200

    Injecting interface for FormBuilder as is good practice in Symfony.

--- src/Metronome/Injection/Mocking/MockBuilder.php
+++ src/Metronome/Injection/Mocking/MockBuilder.php
@@ -6,6 +6,8 @@ use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\ORM\Query\Expr;
 use Metronome\Form\MetronomeFormData;
 use Mockery\MockInterface;
+use Symfony\Bundle\TwigBundle\TwigEngine;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
 use Symfony\Component\Security\Guard\Token\PostAuthenticationGuardToken;
@@ -21,7 +23,7 @@ class MockBuilder
      * @param null $load
      * @param null $loadAll
      * @param null $entityName
-     * @return EntityManager|\Mockery\MockInterface
+     * @return EntityManager|MockInterface
      */
     public static function createMockEntityManager(
         $getRepository = null,
@@ -89,7 +91,7 @@ class MockBuilder
 
     /**
      * @param $getLastAuthenticationError
-     * @return AuthenticationUtils|\Mockery\MockInterface
+     * @return AuthenticationUtils|MockInterface
      */
     public static function createAuthUtilsMock($getLastAuthenticationError) {
         $utilMock = MockCreator::mock('Symfony\Component\Security\Http\Authentication\AuthenticationUtils', array(
@@ -99,36 +101,8 @@ class MockBuilder
     }
 
     /**
-     * @deprecated
-     * @param bool $isSubmitted
-     * @param bool $isValid
-     * @param array $getData
-     * @param array $errors
-     * @return MockInterface
-     */
-    public static function createFormBuilderMock($isSubmitted = false, $isValid = false, $getData = array(), $errors = array()) {
-        $formMock = MockCreator::mock('\Symfony\Component\Form\Form', array(
-            "handleRequest" => null,
-            "isSubmitted" => $isSubmitted,
-            "getData" => $getData,
-            "createView" => null,
-            "isValid" => $isValid,
-            "getErrors" => $errors
-        ));
-        $builderMock = MockCreator::mock('\Symfony\Component\Form\FormBuilderInterface', array(
-            'getForm' => $formMock
-        ));
-
-        $fbMock = MockCreator::mock('\Symfony\Component\Form\FormFactory', array(
-            "create" => $formMock,
-            "createNamedBuilder" => $builderMock
-        ));
-        return $fbMock;
-    }
-
-    /**
      * @param $injectedForms
-     * @return FormFactory|MockInterface
+     * @return FormFactoryInterface|MockInterface
      */
     public static function createFormFactoryMock($injectedForms) {
         $mockForms = array();
@@ -142,24 +116,12 @@ class MockBuilder
             'getForm' => $mockForms[0]
         ));
 
-        $ffMock = MockCreator::mock(FormFactory::class, array(
+        $ffMock = MockCreator::mock(FormFactoryInterface::class, array(
             "createNamedBuilder" => $builderMock
         ));
 
         $ffMock->allows("create")->andReturnValues($mockForms);
         return $ffMock;
-    }
-
-    private static function createFormMock($isValid, $getData, $errors) {
-        $formMock = MockCreator::mock('\Symfony\Component\Form\Form', array(
-            "handleRequest" => null,
-            "isSubmitted" => true,
-            "getData" => $getData,
-            "createView" => null,
-            "isValid" => $isValid,
-            "getErrors" => $errors
-        ));
-        return $formMock;
     }
 
     public static function createTwigEnvironment() {
@@ -180,63 +142,23 @@ class MockBuilder
         return $twigMock;
     }
 
-    /**
-     * @deprecated
-     * @return MockInterface|\Symfony\Bundle\TwigBundle\TwigEngine
-     */
-    public static function createTwigTemplatingMock(){
-        $twigMock = MockCreator::mock('\Symfony\Bundle\TwigBundle\TwigEngine', array(
-            "renderResponse" => new Response(""),
-            "render" => ""
-        ));
-        return $twigMock;
-    }
-
-    /**
-     * @deprecated
-     * @return MockInterface|Twig_Environment
-     * TODO: Improve this rendering method
-     */
-    public static function createTwigMock() {
-        $html = "Test environment prevented template of being rendered";
-        $templateMock = MockCreator::mock('\Twig_Template', array(
-            "render" => $html
-        ));
-        $loaderMock = MockCreator::mock('\Twig_LoaderInterface', array(
-            "exists" => true
-        ));
-
-        $twigMock = MockCreator::mock('\Twig_Environment', array(
-            "disableDebug" => null,
-            "getLoader" => $loaderMock,
-            "loadTemplate" => $templateMock,
-            "render" => $html
-        ));
-        return $twigMock;
-    }
-
-    public static function createRouterMock() {
-        $routerMock = MockCreator::mock('\Symfony\Component\Routing\Router', array(
-            "getRouteCollection" => array(),
-            "generate" => "http://some/url",
-        ));
-        return $routerMock;
-    }
-
-    public static function createMockFileSystem($exists = true)
-    {
-        $mockFS = MockCreator::mock('\Symfony\Component\Filesystem\Filesystem', array(
-            "exists" => $exists,
-            "mkdir" => null,
-        ));
-        return $mockFS;
-    }
-
     public static function createTokenStorageMock(PostAuthenticationGuardToken $token = null) {
         $storageMock = MockCreator::mock('Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage', array(
             'getToken' => $token,
             'setToken' => null
         ));
         return $storageMock;
+    }
+
+    private static function createFormMock($isValid, $getData, $errors) {
+        $formMock = MockCreator::mock('\Symfony\Component\Form\Form', array(
+            "handleRequest" => null,
+            "isSubmitted" => true,
+            "getData" => $getData,
+            "createView" => null,
+            "isValid" => $isValid,
+            "getErrors" => $errors
+        ));
+        return $formMock;
     }
 }
